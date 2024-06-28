3c53923ed378ded96067586d755d225fe7f1cc25
viveoff <vlad.shorodok14@gmail.com>
2024-06-28 08:28:19 +0300

    3

--- 9_Drumuri_Grafuri/functii.py
+++ 9_Drumuri_Grafuri/functii.py
@@ -1,33 +1,24 @@
 def includeNod(nod, lista_vecini, graf):
-    if nod in graf:
+    if nod in lista_vecini:
         graf[nod] += lista_vecini
     else:
         graf[nod] = lista_vecini
 def citire_tastatura():
     graf = dict()
+
     while True:
-        linie = input("Nod: ").strip()
+        linie = input("Nod: ")
         if linie == '':
             break
 
-        nod, vecini = linie.split(":")
+        nod, urm = linie.split(':')
         nod = nod.strip()
-        lista_vecini = [vecin.strip() for vecin in vecini.split(",") if vecin.strip()]
+        lista_vecini = [vec.strip() for vec in urm.split(',') if vec != '']
+
         includeNod(nod, lista_vecini, graf)
+
     return graf
 
-def citire_fisier():
-    graf = dict()
-    set = int(input("Set (1 - 4): "))
-    with open("file.txt", "r") as f:
-        linii = f.readlines()
-        for linie in linii[0:3]:
-            nod, vecini = linie.split(":")
-            nod = nod.strip()
-            lista_vecini = [v.strip() for v in vecini.split(",") if v.strip()]
-            includeNod(nod, lista_vecini, graf)
-    return graf
 def afisare(graf):
     print(graf)
-
 
